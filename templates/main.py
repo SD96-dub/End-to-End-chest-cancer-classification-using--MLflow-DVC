bd2b5d4cbcef2d49ce09930ed07afaed2fe0146d
SD96-dub <0606dubeshreya@gmail.com>
2025-12-22 23:05:26 +0530

    requirements added

--- templates/main.py
+++ templates/main.py
@@ -1,8 +1,7 @@
-import os
-import sys
-
-# add 'src' folder to system path
-sys.path.append(os.path.join(os.getcwd(), "src"))
 
 from cnnClassifier import logger
-logger.info("welcome to cnnClassifier!")
+
+logger.info("Welcome to cnnclassifier")
+
+
+
